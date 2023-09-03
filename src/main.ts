ff1b27efc616deca3a05848b1fb31d75fd6ca435
htpvdev <haruki20211121@gmail.com>
2023-09-03 14:47:40 +0900

    同様のものを実装するまで完了

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,182 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
+import _ from 'lodash'
+
+import questions, { Question } from './config/questions'
+import { returnInputText, setHtml, setStyle, setText, setValue } from './method'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <!-- <audio id="errorSound" src="path_to_your_audio_file/error.mp3" preload="auto"></audio> -->
+
+    <div id="genreSelection">
+      <button id="iphoneGenreBtn">通信</button>
+      <button id="programGenreBtn">プログラミング</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+
+    <div id="gameArea">
+      <div id="countdown"><p>3</p></div>
+      <div id="questionView"><p>...</p></div>
+      <!-- <div id="furiganaView"><p>...</p></div> -->
+      <div id="inputKeyView"><p>...</p></div>
+      <input type="text" id="userInput">
+      <div id="msg"><p>...</p></div>
+    </div>
+
+    <div>
+      <div id="resultView"><p>...</p></div>
+      <button id="restartBtn">再スタート</button>
+    </div>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const msg = [ '焦らず頑張って！', '基礎ができていませんね', 'どうしてこれができないんですか？', 'まずは何ができないのか最初に言ってください。' ];
+
+const questionViewEl = document.querySelector<HTMLDivElement>('#questionView')?.querySelector('p');
+// const furiganaViewEl = document.querySelector<HTMLDivElement>('#furiganaView');
+const inputKeyViewEl = document.querySelector<HTMLDivElement>('#inputKeyView')?.querySelector('p');
+const resultViewEl = document.querySelector<HTMLDivElement>('#resultView')?.querySelector('p');
+const userInputEl = document.querySelector<HTMLInputElement>('#userInput');
+const countdownEl = document.querySelector<HTMLDivElement>('#countdown')?.querySelector('p');
+const gameAreaEl = document.querySelector<HTMLDivElement>('#gameArea');
+const genreSelectionEl = document.querySelector<HTMLDivElement>('#genreSelection');
+const msgEl = document.querySelector<HTMLDivElement>('#msg')?.querySelector('p');
+// const errorSound = document.querySelector<>('#errorSound');
+
+let setQuestions: Question[] = [];
+let currentQuestion = 0;
+let correctCount = 0;
+// let totalAttempts = 0;
+let wrongCharCount = 0;
+let countdownInterval: number | undefined;
+// let afterCurrentTarget = '';
+let currentTarget = '';
+let inputExist = false;
+let redContent = '';
+let newContent = '';
+let remainingTarget = '';
+
+// 最初に表示しない要素を隠す
+if (gameAreaEl?.style) {
+  gameAreaEl.style.visibility = 'none';
+}
+
+
+
+const changeWord = () => {
+  if (currentQuestion < setQuestions.length) {  // 修正：問題数に基づく条件判定
+    setText(questionViewEl, setQuestions[currentQuestion].questionText)
+    // setHtml(furiganaViewEl, setQuestions[currentQuestion].furigana)
+    setHtml(inputKeyViewEl, returnInputText(setQuestions[currentQuestion].furigana))
+    // console.log('changeWord, next text is ', returnInputText(setQuestions[currentQuestion].furigana));
+    currentQuestion++;
+  } else {
+    setStyle(gameAreaEl, 'none')
+    setHtml(resultViewEl, `単語数: ${correctCount} / ${setQuestions.length}<br>間違えた文字数: ${wrongCharCount}`)
+  }
+}
+
+const startCountdown = (callback: () => void) => {
+  let countdownValue = 3;
+  setText(countdownEl, String(countdownValue));
+  countdownInterval = setInterval(() => {
+    countdownValue--;
+    setText(countdownEl, String(countdownValue));
+
+    if (countdownValue < 0) {
+      countdownValue = 3;
+      clearInterval(countdownInterval);
+      setStyle(countdownEl, 'none');
+      callback();
+    }
+  }, 1000);
+}
+
+const startGame = (genre: 'iphone' | 'program') => {
+  setQuestions = questions[genre].sort(() => Math.random() - 0.5).slice(0, 5);
+  currentQuestion = 0;
+  correctCount = 0;
+  setText(questionViewEl, ' ')
+  setText(inputKeyViewEl, ' ')
+  setText(resultViewEl, ' ')
+  setStyle(genreSelectionEl, 'none')
+  setStyle(gameAreaEl, 'flex')
+
+  startCountdown(changeWord);
+}
+
+
+const randomMsg = (wrongCharCount: number) => {
+  if (msg.length < wrongCharCount) {
+    return 'しんじゃえ(*´∀｀) [笑]';
+  }
+  return msg[wrongCharCount - 1];
+}
+
+const restartGame = () => {
+  clearInterval(countdownInterval);  // この行を追加
+
+
+  setStyle(genreSelectionEl, 'flex')
+  setStyle(gameAreaEl, 'none')
+  setStyle(countdownEl, 'block')
+  setText(countdownEl, '3')
+  setText(userInputEl, '')
+  setHtml(inputKeyViewEl, '')
+  setText(questionViewEl, '')
+  setHtml(resultViewEl, '')
+
+  wrongCharCount = 0;
+  correctCount = 0;
+  currentQuestion = 0;
+  // afterCurrentTarget = '';
+  currentTarget = '';
+  inputExist = false;
+  redContent = '';
+  newContent = '';
+}
+
+document.querySelector<HTMLButtonElement>('#iphoneGenreBtn')?.addEventListener('click', () => startGame('iphone'));
+document.querySelector<HTMLButtonElement>('#programGenreBtn')?.addEventListener('click', () => startGame('program'));
+document.querySelector<HTMLButtonElement>('#restartBtn')?.addEventListener('click', restartGame);
+
+userInputEl?.addEventListener('keydown', (e) => {
+  const targetQuestion = returnInputText(setQuestions[currentQuestion - 1].furigana);
+  if (inputExist) {
+    currentTarget = remainingTarget;
+  } else {
+    currentTarget = targetQuestion;  // 修正: 現在の問題の単語を正しく取得
+  }
+  const inputValue = userInputEl?.value;
+  const inputKey = e.key;
+
+  if (inputKey === currentTarget[0]) {
+    inputExist = true;
+    redContent += `<span style="color: red">${currentTarget[0]}</span>`;
+    remainingTarget = currentTarget.slice(1);
+    newContent = redContent + remainingTarget;
+    setHtml(inputKeyViewEl, newContent);
+    setHtml(msgEl, '')
+  } else {
+    wrongCharCount++
+    e.preventDefault()
+
+    setHtml(msgEl, `<span style="color: red">${randomMsg(wrongCharCount)}</span>`)
+
+    setHtml(inputKeyViewEl, inputExist? newContent : targetQuestion);
+  }
+
+  if (targetQuestion === inputValue + e.key || targetQuestion === inputValue) {
+    console.log('正解や！問題変更や！')
+    correctCount++;
+    setValue(userInputEl, '');
+    inputExist = false;
+    currentTarget = ''
+    redContent = '';
+    newContent = '';
+    e.preventDefault();
+    changeWord();
+  }
+});
---
+++ src/method.ts
@@ -0,0 +1,88 @@
+/** @description 副作用のない純粋関数を定義する */
+
+import romaji, { Romaji } from "./config/romaji"
+
+export const setText = (el: HTMLElement|null|undefined, text: string) => {
+  // console.log(text)
+  if (el?.textContent) {
+    el.textContent = text
+  } else {
+    console.log('NULL!!!!!!!!', text)
+  }
+}
+
+export const setHtml = (el: HTMLElement|null|undefined, htmlText: string) => {
+  if (el?.innerHTML) {
+    el.innerHTML = htmlText
+  } else {
+    console.log('NULL!!!!!!!!', htmlText)
+  }
+}
+
+export const setStyle = (el: HTMLElement|null|undefined, style: string) => {
+  if (el?.style) {
+    el.style.display = style
+  }
+}
+
+export const setValue = (el: HTMLInputElement|null, value: string) => {
+  if (el?.value) {
+    el.value = value
+  }
+}
+
+export const returnInputText = (furigana: string) => {
+    const wordList: string[] = [];
+    let skipFlag = false;
+    furigana.split('').map((word, index) => {
+        if (skipFlag) {
+            skipFlag = false;
+            return;
+        }
+
+        let wordRomaji = word;
+
+        if (
+            furigana[index + 1] === 'ァ'
+            || furigana[index + 1] === 'ィ'
+            || furigana[index + 1] === 'ゥ'
+            || furigana[index + 1] === 'ェ'
+            || furigana[index + 1] === 'ォ'
+            || furigana[index + 1] === 'ャ'
+            || furigana[index + 1] === 'ュ'
+            || furigana[index + 1] === 'ョ'
+        ) {
+            wordRomaji = word + furigana[index + 1];
+            skipFlag = true;
+        }
+        wordList.push(wordRomaji);
+    });
+
+    let inputText = '';
+
+    wordList.map((word, index) => {
+        // 「ッ」の直前対応
+        if (word === 'ッ') {
+            inputText += romaji[wordList[index + 1] as Romaji][0].charAt(0);
+            return;
+        }
+        // 「ン」のナ行直前対応
+        if (word === 'ン') {
+            const nextWord = wordList[index + 1][0];
+            if (
+                nextWord === 'ナ'
+                || nextWord === 'ニ'
+                || nextWord === 'ヌ'
+                || nextWord === 'ネ'
+                || nextWord === 'ノ'
+            ) {
+                inputText += 'nn';
+                return;
+            }
+        }
+
+        inputText += (romaji[word as Romaji] ?? [word])[0];
+    });
+
+    return inputText;
+}
